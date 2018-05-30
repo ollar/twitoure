3e0e8b6e795ee3bd3867ba39944610705a886157
Oleg Larkin <olegollar@gmail.com>
2018-05-31 00:50:40 +0300

    models update

--- app/models/image.js
+++ app/models/image.js
@@ -2,14 +2,13 @@ import DS from 'ember-data';
 import { computed } from '@ember/object';
 
 export default DS.Model.extend({
-  url: DS.attr('string'),
-  type: DS.attr('string'),
-  name: DS.attr('string'),
-  size: DS.attr('number'),
-  lastModified: DS.attr('date'),
-  width: DS.attr('number'),
-  height: DS.attr('number'),
-  timestampCreated: DS.attr('number'),
+    url: DS.attr('string'),
+    type: DS.attr('string'),
+    name: DS.attr('string'),
+    size: DS.attr('number'),
+    width: DS.attr('number'),
+    height: DS.attr('number'),
+    timestampCreated: DS.attr('number'),
 
-  base64: computed(() => ''),
+    base64: computed(() => ''),
 });
--- app/models/user.js
+++ app/models/user.js
@@ -2,22 +2,21 @@ import DS from 'ember-data';
 import { computed } from '@ember/object';
 
 export default DS.Model.extend({
-  fingerprint: DS.attr('string'),
+    fingerprint: DS.attr('string'),
 
-  username: DS.attr('string'),
+    username: DS.attr('string'),
 
-  firstName: DS.attr('string'),
-  lastName: DS.attr('string'),
+    firstName: DS.attr('string'),
+    lastName: DS.attr('string'),
 
-  avatar: DS.hasMany('image'),
-  created: DS.attr('date'),
-  timestampCreated: DS.attr('number'),
+    avatar: DS.hasMany('image'),
+    timestampCreated: DS.attr('number'),
 
-  points: DS.hasMany('point'),
+    points: DS.hasMany('point'),
 
-  validations: computed(() => ({
-    username: {
-      presence: true,
-    },
-  })),
+    validations: computed(() => ({
+        username: {
+            presence: true,
+        },
+    })),
 });
